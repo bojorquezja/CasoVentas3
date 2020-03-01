1b3ea1e445d5fee31280098fe2bf17db4ddc88c3
bojorquezja <bojorquez.ja@gmail.com>
2020-03-01 02:41:20 -0500

    primer commit

---
+++ src/main/java/pe/edu/utp/casoventas3/Casoventas3Application.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.casoventas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casoventas3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Casoventas3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/casoventas3/Casoventas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.casoventas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casoventas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
